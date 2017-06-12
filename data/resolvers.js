d12ead8ae074f26ba7d74083fa3fec410652b4b2
Jamel De la Fuente <jamel.fuente@gmail.com>
2017-06-12 23:47:04 +0200

    All the dev files to create a SQL connection

---
+++ data/resolvers.js
@@ -0,0 +1,23 @@
+import { Author } from './connectors';
+
+const resolvers = {
+  Query: {
+    author(_, args) {
+      return Author.find({ where: args });
+    },
+  },
+  Author: {
+    posts(author) {
+      return author.getPosts();
+    },
+  },
+  Post: {
+    author(post) {
+      return post.getAuthor();
+    },
+  },
+};
+
+export default resolvers;
+
+export default resolvers;
